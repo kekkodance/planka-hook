8c12073f92597d4c1bc7fef30692f90c2bd86205
Kekko <53054786+kekkodance@users.noreply.github.com>
2024-10-21 23:22:08 +0200

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,3 +1,10 @@
-        document.addEventListener("DOMContentLoaded", function() {
-            document.title = "hello world";
-        });
+document.addEventListener("DOMContentLoaded", function() {
+    // Change the document title
+    document.title = "hello world";
+
+    // Change the link text if it exists
+    const logoLink = document.querySelector('a.Header_logo__shVBB.Header_title__l+wMf');
+    if (logoLink) {
+        logoLink.innerText = 'TwistedFurby';
+    }
+});
