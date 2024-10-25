fd8e50affe3a581ad93cdb5a01d598c4f2bea5fc
Kekko <53054786+kekkodance@users.noreply.github.com>
2024-10-25 19:51:01 +0200

    Update inject.js

--- inject.js
+++ inject.js
@@ -6,6 +6,10 @@ style.innerHTML = `
 
 //DARK THEME SCHEDE START
 
+
+
+//DARK THEME SCHEDE START
+
 #app .Stopwatch_wrapperHoverable__tpGPq {
   background: #3B3B3B;
   color: #fff;
@@ -96,18 +100,12 @@ style.innerHTML = `
 //DARK THEME SCHEDE END
 
 
-#app .BoardActions_actions__jF5z\+ {
+#app .BoardActions_actions__jF5z+ {
   gap: 0px
 }
 
-[class~=large]>div:nth-child(1)>a:nth-child(1),
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=AccountPane_wrapper__pWfwV],
-.fluid.button.ActionsStep_button__2Cph7,
-.menu a.item.UserStep_menuItem__5pvtT:nth-child(1) {
-    display: none !important;
-}
 
-#app .BoardActions_actions__jF5z\+ {
+#app .BoardActions_actions__jF5z+ {
   gap: 0px
 }
 
@@ -136,6 +134,30 @@ div[class~=ui][class~=segment][class~=active][class~=tab][class~=AccountPane_wra
 .Item_author__Gb7ek {
     visibility: hidden;
     position: relative;
+}
+
+.ItemComment_author__J14e1 {
+    visibility: hidden;
+    position: relative;
+}
+
+.ItemComment_author__J14e1:after {
+    content: "TwistedFurby";
+    visibility: visible;
+    position: absolute;
+    top: 0;
+    left: 0;
+}
+
+.ItemComment_date__azm7i:before {
+    content: "· ";
+    font-weight: 900;
+    font-size: 15px
+}
+
+.ItemComment_date__azm7i {
+    position: relative;
+    left: -75px
 }
 
 .Item_author__Gb7ek::after {
@@ -186,24 +208,10 @@ div[class~=Login_inputWrapper__SbI14]:nth-child(1):before {
     font-size: 12pt;
 }
 
-[class~=Header_logo__shVBB],
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=AccountPane_wrapper__pWfwV],
-.menu a.item.UserStep_menuItem__5pvtT:nth-child(1),
-.fluid.button.ActionsStep_button__2Cph7,
-[class~=large]>div:nth-child(1)>a:nth-child(1) {
-    visibility: hidden !important;
-}
 
 
 [class~=large]>div:nth-child(1)>a:nth-child(1) {
     left: 6pt;
-}
-
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=AccountPane_wrapper__pWfwV],
-.fluid.button.ActionsStep_button__2Cph7,
-.menu a.item.UserStep_menuItem__5pvtT:nth-child(1),
-[class~=large]>div:nth-child(1)>a:nth-child(1) {
-    opacity: 0 !important;
 }
 
 html body#app div#root div.Login_wrapper__zYzT6.Login_fullHeight__Fded8 div.ui.middle.aligned.grid.Login_fullHeightPaddingFix__qqodI div.six.wide.computer.five.large.screen.sixteen.mobile.tablet.four.widescreen.column div.ui.middle.aligned.grid.Login_fullHeightPaddingFix__qqodI div.column div.Login_loginWrapper__XW1w2 div form.ui.large.form div.field button.ui.large.icon.primary.right.floated.labeled.button:hover {
@@ -219,29 +227,9 @@ html body#app div#root div.Login_wrapper__zYzT6.Login_fullHeight__Fded8 div.ui.m
     background: #242424 !important;
 }
 
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=AccountPane_wrapper__pWfwV],
-[class~=large]>div:nth-child(1)>a:nth-child(1),
-.fluid.button.ActionsStep_button__2Cph7,
-.menu a.item.UserStep_menuItem__5pvtT:nth-child(1) {
-    filter: opacity(0) !important;
-}
-
-.menu a.item.UserStep_menuItem__5pvtT:nth-child(1),
-.fluid.button.ActionsStep_button__2Cph7,
-[class~=large]>div:nth-child(1)>a:nth-child(1),
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=AccountPane_wrapper__pWfwV] {
-    color: rgba(0, 0, 0, 0) !important;
-}
 
 a[class~=item]:nth-child(2) {
     user-select: none !important;
-}
-
-[class~=large]>div:nth-child(1)>a:nth-child(1),
-.menu a.item.UserStep_menuItem__5pvtT:nth-child(1),
-.fluid.button.ActionsStep_button__2Cph7,
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=AccountPane_wrapper__pWfwV] {
-    background-color: rgba(0, 0, 0, 0) !important;
 }
 
 [class~=Header_logo__shVBB]:after {
@@ -252,12 +240,6 @@ html body#app div#root div.Login_wrapper__zYzT6.Login_fullHeight__Fded8 div.ui.m
     transition: background .3s ease;
 }
 
-[class~=large]>div:nth-child(1)>a:nth-child(1),
-.menu a.item.UserStep_menuItem__5pvtT:nth-child(1),
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=AccountPane_wrapper__pWfwV],
-.fluid.button.ActionsStep_button__2Cph7 {
-    transform: scale(0) !important;
-}
 
 [class~=Header_logo__shVBB]:after {
     top: 0;
@@ -267,12 +249,7 @@ div[class~=Login_inputWrapper__SbI14]:nth-child(1):before {
     font-weight: 700;
 }
 
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=AccountPane_wrapper__pWfwV],
-.menu a.item.UserStep_menuItem__5pvtT:nth-child(1),
-[class~=large]>div:nth-child(1)>a:nth-child(1),
-.fluid.button.ActionsStep_button__2Cph7 {
-    clip-path: circle(0) !important;
-}
+
 
 [class~=Login_formTitle__xnYyM] {
     padding-top: 1.5625in !important;
@@ -294,11 +271,7 @@ div[class~=Login_inputWrapper__SbI14]:nth-child(1):before {
     line-height: .208333333in;
 }
 
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=AccountPane_wrapper__pWfwV],
-.fluid.button.ActionsStep_button__2Cph7,
-.menu a.item.UserStep_menuItem__5pvtT:nth-child(1) {
-    position: absolute !important;
-}
+
 
 [class~=Header_logo__shVBB]:after {
     content: "TwistedFurby";
@@ -320,26 +293,6 @@ input::selection {
     white-space: nowrap;
 }
 
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=PreferencesPane_wrapper__OLqz6],
-.menu a.item.UserStep_menuItem__5pvtT:nth-child(1),
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=AccountPane_wrapper__pWfwV],
-.fluid.button.ActionsStep_button__2Cph7 {
-    left: -7499.25pt !important;
-}
-
-[class~=large]>div:nth-child(1)>a:nth-child(1),
-.menu a.item.UserStep_menuItem__5pvtT:nth-child(1),
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=AccountPane_wrapper__pWfwV],
-.fluid.button.ActionsStep_button__2Cph7 {
-    height: 0 !important;
-}
-
-[class~=large]>div:nth-child(1)>a:nth-child(1),
-.menu a.item.UserStep_menuItem__5pvtT:nth-child(1),
-.fluid.button.ActionsStep_button__2Cph7,
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=AccountPane_wrapper__pWfwV] {
-    padding-left: 0 !important;
-}
 
 textarea::selection {
     background-color: #f42160;
@@ -349,23 +302,11 @@ textarea::selection {
     overflow: hidden;
 }
 
-.fluid.button.ActionsStep_button__2Cph7,
-.menu a.item.UserStep_menuItem__5pvtT:nth-child(1),
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=AccountPane_wrapper__pWfwV],
-[class~=large]>div:nth-child(1)>a:nth-child(1) {
-    padding-bottom: 0 !important;
-}
 
 html body#app div#root div.Login_wrapper__zYzT6.Login_fullHeight__Fded8 div.ui.middle.aligned.grid.Login_fullHeightPaddingFix__qqodI div.six.wide.computer.five.large.screen.sixteen.mobile.tablet.four.widescreen.column div.ui.middle.aligned.grid.Login_fullHeightPaddingFix__qqodI div.column div.Login_loginWrapper__XW1w2 div form.ui.large.form div.field button.ui.large.icon.primary.right.floated.labeled.button {
     background-color: #fe4678 !important;
 }
 
-[class~=large]>div:nth-child(1)>a:nth-child(1),
-.fluid.button.ActionsStep_button__2Cph7,
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=AccountPane_wrapper__pWfwV],
-.menu a.item.UserStep_menuItem__5pvtT:nth-child(1) {
-    padding-right: 0 !important;
-}
 
 [class~=Login_formTitle__xnYyM] {
     background-position: center;
@@ -387,19 +328,6 @@ a[class~=item]:nth-child(2) {
   margin-top: 20px
 }
 
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=AccountPane_wrapper__pWfwV],
-[class~=large]>div:nth-child(1)>a:nth-child(1),
-.fluid.button.ActionsStep_button__2Cph7,
-.menu a.item.UserStep_menuItem__5pvtT:nth-child(1) {
-    padding-top: 0 !important;
-}
-
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=AccountPane_wrapper__pWfwV],
-.fluid.button.ActionsStep_button__2Cph7,
-.menu a.item.UserStep_menuItem__5pvtT:nth-child(1),
-[class~=large]>div:nth-child(1)>a:nth-child(1) {
-    overflow: hidden !important;
-}
 
 h2[class~=ui][class~=inverted][class~=header][class~=Login_descriptionSubtitle__F9udW]:before {
     content: "Powered by Ripped.guide";
@@ -434,10 +362,6 @@ h2.ui.inverted.header.Login_descriptionSubtitle__F9udW {
     visibility: hidden;
 }
 
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=PreferencesPane_wrapper__OLqz6],
-span[class~=Filters_filter__Y8tDL]:nth-child(1)>button:nth-child(1) {
-    display: none !important;
-}
 
 [class~=Login_cover__6nzRu] {
     background-repeat: no-repeat !important;
@@ -455,25 +379,12 @@ div[class~=Login_inputWrapper__SbI14]:nth-child(2):before {
     display: none
 }
 
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=PreferencesPane_wrapper__OLqz6] {
-    visibility: hidden !important;
-}
-
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=PreferencesPane_wrapper__OLqz6] {
-    opacity: 0 !important;
-}
-
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=PreferencesPane_wrapper__OLqz6] {
-    filter: opacity(0) !important;
-}
 
 input {
     background: #191919 !important;
 }
 
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=PreferencesPane_wrapper__OLqz6] {
-    color: rgba(0, 0, 0, 0) !important;
-}
+
 
 div[class~=Login_inputWrapper__SbI14]:nth-child(2)>div:nth-child(1),
 div[class~=Login_inputWrapper__SbI14]:nth-child(1)>div:nth-child(1),
@@ -485,64 +396,43 @@ div[class~=Login_inputWrapper__SbI14]:nth-child(2):before {
     font-weight: 700;
 }
 
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=PreferencesPane_wrapper__OLqz6] {
-    background-color: rgba(0, 0, 0, 0) !important;
-}
+
 
 [class~=Header_logo__shVBB]:after {
     font-size: clamp(16px, 3vw, 20px);
 }
 
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=PreferencesPane_wrapper__OLqz6] {
-    transform: scale(0) !important;
-}
+
 
 div.Board_lists__gV2Uz {
     flex-wrap: wrap;
 }
 
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=PreferencesPane_wrapper__OLqz6] {
-    clip-path: circle(0) !important;
-}
 
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=PreferencesPane_wrapper__OLqz6] {
-    position: absolute !important;
-}
 
 [class~=Header_item__OOEY7][class~=Header_itemHoverable__nf6CM]:nth-child(1) {
     padding-left: 0pt;
 }
 
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=PreferencesPane_wrapper__OLqz6] {
-    height: 0 !important;
-}
+
 
 textarea::selection {
     color: #fff;
 }
 
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=PreferencesPane_wrapper__OLqz6] {
-    padding-left: 0 !important;
-}
+
 
 [class~=Header_item__OOEY7][class~=Header_itemHoverable__nf6CM]:nth-child(1) {
     padding-bottom: 0pt;
 }
 
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=PreferencesPane_wrapper__OLqz6] {
-    padding-bottom: 0 !important;
-}
-
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=PreferencesPane_wrapper__OLqz6] {
-    padding-right: 0 !important;
-}
 
 div[class~=Login_inputWrapper__SbI14]:nth-child(2):before {
     line-height: 1.25pc;
 }
 
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=PreferencesPane_wrapper__OLqz6] {
-    padding-top: 0 !important;
+#app .AccountPane_wrapper__pWfwV {
+    padding-top: 0px;
 }
 
 [class~=Login_cover__6nzRu] {
@@ -557,8 +447,16 @@ div[class~=Login_inputWrapper__SbI14]:nth-child(2):before {
     position: relative;
 }
 
-div[class~=ui][class~=segment][class~=active][class~=tab][class~=PreferencesPane_wrapper__OLqz6] {
-    overflow: hidden !important;
+html body#app.dimmable.dimmed div.ui.top.aligned.page.modals.dimmer.transition.visible.active div.ui.small.modal.transition.visible.active div.content div div.ui.segment.active.tab.AccountPane_wrapper__pWfwV form.ui.form {
+    display: none;
+}
+
+html body#app.dimmable.dimmed div.ui.top.aligned.page.modals.dimmer.transition.visible.active div.ui.small.modal.transition.visible.active div.content div div.ui.segment.active.tab.AccountPane_wrapper__pWfwV button.User_button__CgzK9 {
+    display: none;
+}
+
+html body#app.dimmable.dimmed div.ui.top.aligned.page.modals.dimmer.transition.visible.active div.ui.small.modal.transition.visible.active div.content div div.ui.segment.active.tab.AccountPane_wrapper__pWfwV br {
+    display: none;
 }
 
 #app a.item:nth-child(3) {
