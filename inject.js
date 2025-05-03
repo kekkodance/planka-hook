417fe2d5f31adba377e84ec6a886b08f0c987bd6
Kekko <53054786+kekkodance@users.noreply.github.com>
2025-05-03 23:04:20 +0200

    push

--- inject.js
+++ inject.js
@@ -12,6 +12,12 @@ style.innerHTML = `
 .ui.menu .item {
     color: #ffffffb8
 }
+
+.ui.secondary.menu .active.item {
+    color: #fff;
+    background-color: #101010
+    
+}
 .ActionsStep_menuItem__3UZGJ:hover {
     background: #303030 !important;
     color: #ffffffb8 !important;
@@ -38,14 +44,16 @@ div.ui.bottom.left.wide.basic.popup.transition.visible.Popup_wrapper__gjV0g {
 }
 
 button.ui.button.EditStep_deleteButton__NTHkI,
-button.ui.button.EditStep_deleteButton__a03v5 {
+button.ui.button.EditStep_deleteButton__a03v5,
+#app .EditStep_deleteButton__EiWg\+ {
     color: #fff;
     background-color: #cf513d;
     text-shadow: none;
     box-shadow: 0 1px 0 #ae2e2e !important;
 }
 button.ui.button.EditStep_deleteButton__NTHkI:hover,
-button.ui.button.EditStep_deleteButton__a03v5:hover {
+button.ui.button.EditStep_deleteButton__a03v5:hover,
+#app .EditStep_deleteButton__EiWg\+:hover {
     background-color: #b04632;
 }
 
@@ -135,7 +143,15 @@ button.ui.button.EditStep_deleteButton__a03v5:hover {
 #app .Activities_toggleButton__VpLFR:hover,
 #app .ItemComment_author__J14e1,
 #app .Item_author__Gb7ek,
-#app .Item_option__5o2ZW:hover {
+#app .Item_option__5o2ZW:hover,
+.ui.basic.table th,
+.ui.modal > .header,
+#app .AddStep_importButton__Qvl87:hover,
+#app .ImportStep_button__oBffs:hover,
+#app .GeneralPane_actionButton__QZx1A:hover,
+#app .BackgroundPane_actionButton__H5sjC:hover,
+#app .AvatarEditStep_actionButton__TsBby:hover,
+#app .UserItem_menuItemText__EuvLW:hover {
     color: #ffffffb8
 }
 
@@ -152,7 +168,21 @@ button.ui.button.EditStep_deleteButton__a03v5:hover {
 #app .CardModal_text__SCB7P,
 #app .Editor_text__zolDF,
 #app .EditStep_text__VO1tP,
-#app .Card_attachmentContent__9uvnk {
+#app .Card_attachmentContent__9uvnk,
+#app .AboutPane_version__iVJbU,
+.ui.basic.table td,
+#app .UserInformationEdit_text__KQgCF,
+#app .UserEmailEditStep_text__KxLk5,
+#app .UserPasswordEditStep_text__bCLOO,
+#app .InformationEdit_text__9Q6-S,
+#app .AddStep_importButton__Qvl87,
+#app .ImportStep_button__oBffs,
+#app .Filters_searchInactive__UE63- input::placeholder,
+#app .GeneralPane_actionButton__QZx1A,
+#app .UserItem_menuItemTextActive__I2\+Yg,
+#app .BackgroundPane_actionButton__H5sjC,
+#app .AvatarEditStep_actionButton__TsBby,
+#app .UserItem_menuItemText__EuvLW {
     color: #aaa
 }
 
@@ -163,7 +193,8 @@ button.ui.button.EditStep_deleteButton__a03v5:hover {
 }
 #app .Tasks_taskButton__92Nn1:hover,
 #app .Item_textEditable__U+Pwe:hover,
-#app .CardModal_descriptionButton__-zAfk:hover {
+#app .CardModal_descriptionButton__-zAfk:hover,
+#app .AvatarEditStep_actionButton__TsBby:hover {
     background-color: #303030
 }
 
@@ -222,6 +253,13 @@ button.ui.button.EditStep_deleteButton__a03v5:hover {
     min-height: 40px
 }
 
+#app .GeneralPane_actionButton__QZx1A:hover {
+    color: #fff;
+    background-color: #cf513d;
+    text-shadow: none;
+    box-shadow: 0 1px 0 #ae2e2e !important;
+}    
+    
 #app .Add_field__bkJN2 {
     background-color: #121212;
     color: #ffffffb8;
@@ -269,6 +307,14 @@ html body#app .CardModal_attachment__ogpcE button.Stopwatch_button__3iHGS span.S
     fill: #fff;
 }
 
+#app .NameEdit_field__rC9xs,
+#app .NameEdit_field__rC9xs textarea,
+.ui.form textarea {
+    background-color: #121212 !important;
+    color: #fff !important;
+    fill: #fff;
+}
+
 #app .ListAdd_wrapper__01Vmd {
     background-color: #1b1b1b
 }
@@ -280,17 +326,49 @@ html body#app .CardModal_attachment__ogpcE button.Stopwatch_button__3iHGS span.S
 }
     
 
-#app .CardModal_actionButton__1eB1l {
+
+
+body > div.ui.page.modals.dimmer.transition.visible.active[style*="display: flex"] 
+> div.ui.tiny.basic.modal.transition.visible.active 
+> div.content {
+    padding-bottom: 3.5rem !important;
+}
+
+
+#app .CardModal_actionButton__1eB1l,
+#app .Item_button__\+54eA {
     background-color: #121212;
     color: #aaa;
     fill: #fff;
+}
+
+#app .BackgroundPane_actionButton__H5sjC:hover,
+#app .BackgroundPane_action__f23lf {
+    background-color: #1b1b1b !important;
+    border: none !important;
+    box-shadow: none !important;
+}
+
+#app .AvatarEditStep_action__uLlR3,
+#app .AvatarEditStep_action__uLlR3:hover,
+#app .AvatarEditStep_actionButton__TsBby {
+    background-color: #121212 !important;
+}
+
+.ui.modal {
+    background-color: unset
+}
+
+#app .Item_button__\+54eA {
+    box-shadow: unset
 }
 
 #app .CardModal_actionIcon__dnocs {
     color: #fff
 }
 
-#app .CardModal_actionButton__1eB1l:hover {
+#app .CardModal_actionButton__1eB1l:hover,
+#app .Item_button__\+54eA:hover {
     background-color: #121212;
     color: #fff;
 }
@@ -299,6 +377,10 @@ html body#app .CardModal_attachment__ogpcE button.Stopwatch_button__3iHGS span.S
     background-color: #303030;
     color: #fff;
     fill: #fff;
+}
+
+#app .ItemComment_title__8-\+o2 {
+    background-color: #1b1b1b
 }
 
 
@@ -323,10 +405,42 @@ html body#app .CardModal_attachment__ogpcE button.Stopwatch_button__3iHGS span.S
   fill: #fff;
 }
 
+.ui.modal {
+    border-radius: 1rem !important;
+}
+
 .ui.modal > .close {
   color: #ffffffe8;
   fill: #ffffffe8;
 }
+
+.ui.modal > .content,
+.ui.large.modal > .header,
+.ui.modal > .actions {
+  background: #1b1b1b !important;
+}
+
+.ui.segment {
+    background-color: unset !important;
+}
+
+#app .User_button__CgzK9 {
+    margin-top: 40px !important;
+}
+
+.ui.header {
+    color: #aaa !important;
+}
+
+.ui.input.error > input {
+    background-color: unset !important;
+}
+
+
+.ui.secondary.pointing.menu .active.item {
+    color: #fff !important;
+}
+
 
 .ui.modal > .close:hover,
 #app .Item_editButton__fmRvU:hover {
@@ -339,12 +453,17 @@ html body#app .CardModal_attachment__ogpcE button.Stopwatch_button__3iHGS span.S
     background-color: #1b1b1b;
 }
 
-.ui.checkbox input:checked ~ .box::after, .ui.checkbox input:checked ~ label::after {
+div.ui.segment.active.tab.PreferencesPane_wrapper__OLqz6
+  > div.ui.toggle.checkbox
+  > input:checked ~ label::after {
   color: #fff;
   background-color: #1b1b1b;
-  border: 0.15rem solid #aaa;
-  border-radius: .15rem;
-  position: relative;
+  border: 11px solid #fff;
+}
+
+
+.ui.checkbox input:checked ~ .box::after, .ui.checkbox input:checked ~ label::after {
+    color: #ffffffb8
 }
 
 .ui.checkbox input:checked ~ .box:hover::after, .ui.checkbox input:checked ~ label:hover::after {
@@ -353,15 +472,14 @@ html body#app .CardModal_attachment__ogpcE button.Stopwatch_button__3iHGS span.S
 
 
 .ui.checkbox .box::before, .ui.checkbox label::before {
-  background-color: #1b1b1b !important;
+  background-color: #303030 !important;
   border: 0.15rem solid #aaa;
   border-radius: .15rem;
 }
 
 .ui.checkbox .box:hover::before, .ui.checkbox label:hover::before {
-  background-color: #1b1b1b !important;
+  background-color: #252525 !important;
   border: 0.15rem solid #ffffffb8;
-  border-radius: .15rem;
 }
 
 
@@ -535,33 +653,6 @@ div.DueDateEditStep_fieldBox__vdgwg > div > input {
   transition: transform 0.6s cubic-bezier(0.25, 0.8, 0.25, 1);
 }
 
-button.UserItem_menuItem__+CUNh:nth-child(2) > div:nth-child(2) {
-    visibility: hidden;
-    position: relative
-}
-
-button.UserItem_menuItem__+CUNh:nth-child(2) > div:nth-child(2):before {
-  visibility: visible
-}
-
-button.UserItem_menuItem__+CUNh:nth-child(2) > div:nth-child(2)::after {
-    content: "TwistedFurby";
-    visibility: visible;
-    position: absolute;
-    top: 0;
-    left: 0;
-}
-
-.Item_author__Gb7ek {
-    visibility: hidden;
-    position: relative;
-}
-
-.ItemComment_author__J14e1,
-a.Item_menuItem__wTAgE:nth-child(2) > div:nth-child(2) {
-    visibility: hidden;
-    position: relative;
-}
 
 #app .Item_menuItemTextActive__rkkjJ::before {
     visibility: visible !important;
@@ -576,14 +667,6 @@ a.Item_menuItem__wTAgE:nth-child(2) > div:nth-child(2) {
     color: #ffffffb8
 }
 
-.ItemComment_author__J14e1:after,
-a.Item_menuItem__wTAgE:nth-child(2) > div:nth-child(2):after {
-    content: "TwistedFurby";
-    visibility: visible;
-    position: absolute;
-    top: 0;
-    left: 0;
-}
 
 .segment > button:nth-child(1) {
     display: block !important;
@@ -597,22 +680,6 @@ a.Item_menuItem__wTAgE:nth-child(2) > div:nth-child(2):after {
     font-size: 15px
 }
 
-.ItemComment_date__azm7i {
-    position: relative;
-    left: -75px
-}
-
-.Item_author__Gb7ek::after {
-    content: "TwistedFurby";
-    visibility: visible;
-    position: absolute;
-    top: 0;
-    left: 0;
-}
-.Item_text__ASgDb {
-  position: relative;
-  left: -74px
-}
 
 
 div[class~=Login_inputWrapper__SbI14]:nth-child(1):before {
@@ -674,7 +741,7 @@ html body#app div#root div.Login_wrapper__zYzT6.Login_fullHeight__Fded8 div.ui.m
     transition: background .3s ease;
 }
 
-div.icon > input:nth-child(1), div.Login_inputWrapper__SbI14:nth-child(1) > div:nth-child(2) > input:nth-child(1) {
+.ui.input > input {
     background-color: #0e0e0e !important;
 }
 
@@ -698,6 +765,20 @@ html body#app div#root div[class~=Login_wrapper__zYzT6][class~=Login_fullHeight_
     content: "You made a typo. Try again.";
 }
 
+.Header_userName__r-MQo {
+  color: transparent !important;
+  position: relative !important;
+}
+
+.Header_userName__r-MQo::after {
+  content: "Powered by kekko" !important;
+  position: absolute !important;
+  top: 0 !important;
+  left: 0 !important;
+  color: #ffffffb8;
+  white-space: nowrap;
+}
+
 [class~=Login_formTitle__xnYyM] {
     background-repeat: no-repeat;
 }
@@ -715,7 +796,7 @@ div[class~=Login_inputWrapper__SbI14]:nth-child(1):before {
 }
 
 input::selection {
-    color: #fff;
+    color: #cc3131;
 }
 
 .ui.dimmer {
@@ -743,6 +824,12 @@ html body#app div#root div.Login_wrapper__zYzT6.Login_fullHeight__Fded8 div.ui.m
 }
 
 
+.ui.bottom.right.popup,
+.ui.bottom.left.popup {
+    box-shadow: none !important;
+}
+
+
 [class~=Login_formTitle__xnYyM] {
     background-position: center;
 }
@@ -755,17 +842,13 @@ html body#app div#root div.Login_wrapper__zYzT6.Login_fullHeight__Fded8 div.ui.m
     background: rgba(0, 0, 0, .5);
 }
 
-a[class~=item]:nth-child(2) {
-    font-size: clamp(10px, 3vw, 15px);
-}
-
 .Board_lists__gV2Uz {
   margin-top: 20px
 }
 
 
 h2[class~=ui][class~=inverted][class~=header][class~=Login_descriptionSubtitle__F9udW]:before {
-    content: "Powered by Ripped.guide";
+    content: "Powered by kekko";
 }
 
 div.Login_inputWrapper__SbI14:nth-child(2) {
@@ -789,7 +872,8 @@ div[class~=Login_inputWrapper__SbI14]:nth-child(2):before {
 }
 
 [class~=Login_cover__6nzRu] {
-    background: url("https://furarchiver.net/File/View?artist=twistedfurby&filename=1701454459.twistedfurby_aida_and_maria_stink.png") !important;
+    background: url("https://i.postimg.cc/9fmyY8vN/Susies-fun-1.png") !important;
+    background-position: -250px !important;
 }
 
 h1.ui.inverted.header.Login_descriptionTitle__db6ho,
@@ -814,7 +898,7 @@ div[class~=Login_inputWrapper__SbI14]:nth-child(2):before {
 
 
 input {
-    background: #1a1a1a !important;
+    background:   !important;
 }
 
 
@@ -872,13 +956,23 @@ div[class~=Login_inputWrapper__SbI14]:nth-child(2):before {
 
 #app .Header_userName__r-MQo {
     padding: 0;
-    margin-left: -13px;
-    margin-right: -2px
+    margin-left: -17px;
+    margin-right: 32px;
+}
+
+#app .Header_userName__r-MQo:hover:after {
+    color: #fff !important;
 }
 
 [class~=Header_item__OOEY7][class~=Header_itemHoverable__nf6CM]:nth-child(1) {
     padding-right: 0pt;
 }
+
+#app .Tasks_countOpened__pj2KM::after,
+#app .Tasks_countClosed__7mQLh::after {
+  filter: brightness(500%) contrast(100%);
+}
+
 
 div[class~=Login_inputWrapper__SbI14]:nth-child(2):before {
     bottom: 3pt;
